e5e140f42dd1e95c3f67e6af7102ebf64c8c6189
Austin Carter <98193003+auscarter17@users.noreply.github.com>
2022-03-29 16:45:33 -0400

    added profile data functions

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const printProfileData = profileDataArr => {
         console.log(profileDataArr[i]);
     }
 
-    cconsole.log('===============');
+    console.log('===============');
 
     // Is the same as this...
     profileDataArr.forEach(profileItem => console.log(profileItem));
