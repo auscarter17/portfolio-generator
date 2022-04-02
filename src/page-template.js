46542007419c3b302b016044da05eef6765d177f
Austin Carter <98193003+auscarter17@users.noreply.github.com>
2022-04-01 23:33:51 -0400

    added site generating JS file

--- src/page-template.js
+++ src/page-template.js
@@ -7,7 +7,7 @@ const generateAbout = aboutText => {
 
     return `
         <section class="my-3" id="about">
-            <h2 class="text-dark-bg-primary p-2 display-inline-block">About Me</h2>
+            <h2 class="text-dark bg-primary p-2 display-inline-block">About Me</h2>
             <p>${aboutText}</p>
         </section>
     `;
